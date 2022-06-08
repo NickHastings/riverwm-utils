d063ebce5c351b24fb47064da95305517f142736
Nick Hastings <nicholaschastings@gmail.com>
2022-06-09 07:05:14 +0900

    Style cleanup prompted by flake8

--- riverwm_utils/riverwm_utils.py
+++ riverwm_utils/riverwm_utils.py
@@ -4,26 +4,29 @@ from pywayland.protocol.wayland import WlOutput
 from pywayland.protocol.wayland import WlSeat
 try:
     from pywayland.protocol.river_control_unstable_v1 import ZriverControlV1
-    from pywayland.protocol.river_status_unstable_v1 import ZriverStatusManagerV1
+    from pywayland.protocol.river_status_unstable_v1 import ZriverStatusManagerV1  # noqa: E501
 except ModuleNotFoundError:
-    error_text='''
-    Your pywayland package does not have bindings for river-control-unstable-v1
-    and/or river-status-unstable-v1.
-    These bindings can be generated with the following command:
+    ERROR_TEXT = ('''
+  Your pywayland package does not have bindings for river-control-unstable-v1
+  and/or river-status-unstable-v1.
+  These bindings can be generated with the following command:
 
-    python3 -m pywayland.scanner -i /usr/share/wayland/wayland.xml /usr/share/river-protocols/river-control-unstable-v1.xml  /usr/share/river-protocols/river-status-unstable-v1.xml
+  python3 -m pywayland.scanner -i /usr/share/wayland/wayland.xml '''
+                  '/usr/share/river-protocols/river-control-unstable-v1.xml '
+                  '''/usr/share/river-protocols/river-status-unstable-v1.xml
 
-    Adjust the path of /usr/share/river-protocols/ as approriate for your installation.
-'''
+  Adjust the path of /usr/share/river-protocols/ as approriate for your '''
+                  'installation.')
 
-    print(error_text)
-    quit()
+    print(ERROR_TEXT)
+    sys.exit()
 
 STATUS_MANAGER = None
 CONTROL = None
 
 OUTPUTS = []
 SEAT = None
+
 
 class Output(object):
     def __init__(self):
@@ -38,13 +41,13 @@ class Output(object):
             self.status.destroy()
 
     def configure(self):
-        global STATUS_MANAGER
         self.status = STATUS_MANAGER.get_river_output_status(self.wl_output)
         self.status.user_data = self
         self.status.dispatcher["focused_tags"] = self.handle_focused_tags
 
     def handle_focused_tags(self, output_status, tags):
         self.focused_tags = tags
+
 
 class Seat(object):
     def __init__(self):
@@ -60,21 +63,19 @@ class Seat(object):
             self.status.destroy()
 
     def configure(self):
-        global STATUS_MANAGER
         self.status = STATUS_MANAGER.get_river_seat_status(self.wl_seat)
         self.status.user_data = self
         self.status.dispatcher["focused_output"] = self.handle_focused_output
 
-    def handle_focused_output(self, seat_status, wl_output):
-        global OUTPUTS
+    def handle_focused_output(self, _, wl_output):
         for output in OUTPUTS:
             if output.wl_output == wl_output:
                 self.focused_output = output
 
+
 def registry_handle_global(registry, id, interface, version):
     global STATUS_MANAGER
     global CONTROL
-    global OUTPUTS
     global SEAT
 
     if interface == 'zriver_status_manager_v1':
@@ -92,7 +93,7 @@ def registry_handle_global(registry, id, interface, version):
             SEAT.wl_seat = registry.bind(id, WlSeat, version)
 
 
-USAGE='''usage: cycle-focused-tags [DIRECTION] [NTAGS]
+USAGE = '''usage: cycle-focused-tags [DIRECTION] [NTAGS]
 
 Change to either the next or previous focused tags.
 
@@ -103,6 +104,7 @@ back to the first tag or to the last tag from the first tag.
 If NTAGS is ommiteed, 32 is assumed if both arguments are ommitted
 'next' is used as the DIRECTION.
 '''
+
 
 def cycle_focused_tags():
     n_tags = 32
@@ -129,14 +131,15 @@ def cycle_focused_tags():
 
     if STATUS_MANAGER is None:
         print("Failed to bind river status manager")
-        quit()
+        sys.exit()
 
     if CONTROL is None:
         print("Failed to bind river control")
-        quit()
+        sys.exit()
 
-    # Configuring all outputs, even the ones we do not care about, should be faster
-    # than first waiting for river to advertise the focused output of the SEAT.
+    # Configuring all outputs, even the ones we do not care about,
+    # should be faster than first waiting for river to advertise the
+    # focused output of the SEAT.
     for output in OUTPUTS:
         output.configure()
 
@@ -156,7 +159,8 @@ def cycle_focused_tags():
         new_tags |= (tags << 1)
 
     else:
-        # If lowest bit is set (first tag) => unset it and set last_tag bit on new tags
+        # If lowest bit is set (first tag) => unset it and set
+        # last_tag bit on new tags
         if (tags & 1) != 0:
             tags ^= 1
             new_tags = last_tag
@@ -164,7 +168,7 @@ def cycle_focused_tags():
         new_tags |= (tags >> 1)
 
     CONTROL.add_argument("set-focused-tags")
-    CONTROL.add_argument( str(new_tags) )
+    CONTROL.add_argument(str(new_tags))
     CONTROL.run_command(SEAT.wl_seat)
 
     display.dispatch(block=True)
@@ -176,7 +180,6 @@ def cycle_focused_tags():
 
     if STATUS_MANAGER is not None:
         STATUS_MANAGER.destroy()
-
 
     if CONTROL is not None:
         CONTROL.destroy()
